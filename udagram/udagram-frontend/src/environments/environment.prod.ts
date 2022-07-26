3a18d0e8bccc8af7bc7f92d918c64c03359e1aca
AhmedALSuwayni <ahmedsuwayni@gmail.com>
2022-07-26 11:05:53 +0300

    ct

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-yvsxy7sk.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api-dev.eba-i42ur4m2.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
